50563e8529a1bb33151c86a6a51f9cd879f13234
Max Magee <max.magee@singlewire.com>
2022-08-01 14:57:04 -0500

    Working react-native-reanimated drawer example

---
+++ windows/blank_win/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::blank_win::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::blank_win::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
---
+++ windows/blank_win/ReactPackageProvider.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "winrt/Microsoft.ReactNative.h"
+
+namespace winrt::blank_win::implementation
+{
+    struct ReactPackageProvider : winrt::implements<ReactPackageProvider, winrt::Microsoft::ReactNative::IReactPackageProvider>
+    {
+    public: // IReactPackageProvider
+        void CreatePackage(winrt::Microsoft::ReactNative::IReactPackageBuilder const &packageBuilder) noexcept;
+    };
+} // namespace winrt::blank_win::implementation
+
